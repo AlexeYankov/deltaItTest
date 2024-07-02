8dc73fe968d772611b5b160f6eb5341bcdf60757
Alexey Yankov <yankovav.wm@gmail.com>
2024-07-02 14:11:03 +0300

    addEventListener test capture

--- src/widgets/header/profileInfo/profileInfo.tsx
+++ src/widgets/header/profileInfo/profileInfo.tsx
@@ -5,11 +5,35 @@ type ProfileType = {
   data: { name: string; avatar: string };
 };
 
+const listener = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+  console.log(e.currentTarget.id);
+  console.log('click');
+  console.time('test');
+};
+
+const handler = () => {
+  console.timeEnd('test');
+};
+
+document
+  .getElementById('2')
+  ?.addEventListener('click', (e) => {
+    console.log('before deep')
+    e.stopPropagation()
+  }, {
+    capture: true,
+  });
+
 export const ProfileInfo = ({ data: { name, avatar } }: ProfileType) => {
   return (
     <div className={s.wrapper}>
-      <div className={s.avatar_container}>
-        <img src={avatar} alt="user_avatar" />
+      <div className={s.avatar_container} id="1" onClick={handler}>
+        <img
+          src={avatar}
+          alt="user_avatar"
+          id="2"
+          onClick={(e) => listener(e)}
+        />
         <div className={s.profile__data_container}>{name}</div>
       </div>
       <div className={s.buttonWrapper}>
